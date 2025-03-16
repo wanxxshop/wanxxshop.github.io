880b848dc9a62229ffb8e3e3700eec9ca32e151a
wanxxshop <timcekgarenafreefire@gmail.com>
2025-03-16 07:44:45 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let slideIndex = 0;
+function showSlides() {
+    let slides = document.querySelectorAll(".slider img");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
+document.addEventListener("DOMContentLoaded", showSlides);
